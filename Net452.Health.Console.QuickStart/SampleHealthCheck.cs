9c1379fd4c8a71ffcf243860a515b00b02e715a5
alhardy <al_hardy@live.com.au>
2017-10-21 01:47:05 +1100

    adding net461 sample health console using ms di

---
+++ Net452.Health.Console.QuickStart/SampleHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using App.Metrics.Health;
+
+namespace Net452.Health.Console.QuickStart
+{
+    public class SampleHealthCheck : HealthCheck
+    {
+        public SampleHealthCheck()
+            : base("Sample Health Check") { }
+
+        protected override ValueTask<HealthCheckResult> CheckAsync(CancellationToken cancellationToken = default)
+        {
+            if (DateTime.UtcNow.Second <= 20)
+            {
+                return new ValueTask<HealthCheckResult>(HealthCheckResult.Degraded());
+            }
+
+            if (DateTime.UtcNow.Second >= 40)
+            {
+                return new ValueTask<HealthCheckResult>(HealthCheckResult.Unhealthy());
+            }
+
+            return new ValueTask<HealthCheckResult>(HealthCheckResult.Healthy());
+        }
+    }
+}
--- Net461.Health.MicrosoftDI.Console.QuickStart/Program.cs
+++ Net461.Health.MicrosoftDI.Console.QuickStart/Program.cs
@@ -1,18 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using App.Metrics.Health;
 using App.Metrics.Health.Builder;
-using static System.Console;
+using App.Metrics.Health.Formatters;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Net452.Health.Console.QuickStart
+namespace Net461.Health.MicrosoftDI.Console.QuickStart
 {
     public class Program
     {
         private static async Task Main(string[] args)
         {
-            var health = new HealthBuilder()
+            var services = new ServiceCollection();
+            var healthBuilder = new HealthBuilder()
+                .HealthChecks.RegisterFromAssembly(services)
                 .HealthChecks.AddCheck<SampleHealthCheck>()
                 .HealthChecks.AddCheck("Healthy Check",
                     () => new ValueTask<HealthCheckResult>(HealthCheckResult.Healthy()))
@@ -27,18 +32,23 @@ namespace Net452.Health.Console.QuickStart
                 .HealthChecks.AddHttpGetCheck("github", new Uri("https://github.com/"), TimeSpan.FromSeconds(10))
                 .Build();
 
+            services.AddHealth(healthBuilder);
 
+            var provider = services.BuildServiceProvider();
 
-            var healthStatus = await health.HealthCheckRunner.ReadAsync();
+            var healthCheckRunner = provider.GetRequiredService<IRunHealthChecks>();
+            var healthFormatters = provider.GetRequiredService<IReadOnlyCollection<IHealthOutputFormatter>>();
+
+            var healthStatus = await healthCheckRunner.ReadAsync();
 
             using (var stream = new MemoryStream())
             {
-                await health.DefaultOutputHealthFormatter.WriteAsync(stream, healthStatus);
+                await healthFormatters.First().WriteAsync(stream, healthStatus);
                 var result = Encoding.UTF8.GetString(stream.ToArray());
-                WriteLine(result);
+                System.Console.WriteLine(result);
             }
 
-            ReadKey();
+            System.Console.ReadKey();
         }
     }
 }
