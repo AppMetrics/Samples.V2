e4d388e8b65dd2cb92e557c34dfa1b55e2054a60
alhardy <alhardy@users.noreply.github.com>
2018-04-27 20:46:09 +1000

    AppMetrics/AppMetrics#255 demo metrics hosting configuration in startup.cs

--- AspNetCore2.Api.QuickStart/Program.cs
+++ AspNetCore2.Api.QuickStart/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using App.Metrics;
-using App.Metrics.AspNetCore;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace AspNetCore2.Api.QuickStart
@@ -15,25 +12,6 @@ namespace AspNetCore2.Api.QuickStart
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-#if REPORTING
-                .ConfigureMetricsWithDefaults(builder =>
-                {
-                    builder.Report.ToConsole(TimeSpan.FromSeconds(2));
-                })
-#endif
-#if HOSTING_OPTIONS
-                .ConfigureAppMetricsHostingConfiguration(options =>
-                {
-                    // options.AllEndpointsPort = 3333;
-                    options.EnvironmentInfoEndpoint = "/my-env";
-                    options.EnvironmentInfoEndpointPort = 1111;
-                    options.MetricsEndpoint = "/my-metrics";
-                    options.MetricsEndpointPort = 2222;
-                    options.MetricsTextEndpoint = "/my-metrics-text";
-                    options.MetricsTextEndpointPort = 3333;
-                })
-#endif
-                .UseMetrics()
                 .UseStartup<Startup>()
                 .Build();
     }
--- AspNetCore2.Api.QuickStart/Startup.cs
+++ AspNetCore2.Api.QuickStart/Startup.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using App.Metrics;
+using App.Metrics.AspNetCore.Endpoints;
+using App.Metrics.AspNetCore.Tracking;
+using App.Metrics.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -17,11 +22,25 @@ namespace AspNetCore2.Api.QuickStart
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // TODO: Shouldn't have to register this explicitly, read on AddMetricsEndpoints(Configuration).
+            services.Configure<MetricsEndpointsHostingOptions>(Configuration.GetSection(nameof(MetricsEndpointsHostingOptions)));
+
+            var metrics = AppMetrics.CreateDefaultBuilder()
+                .Configuration.ReadFrom(Configuration)
+                .Report.ToConsole(TimeSpan.FromSeconds(2))
+                .Build();
+
+            services.AddMetrics(metrics);
+            services.AddMetricsTrackingMiddleware(Configuration);
+            services.AddMetricsEndpoints(Configuration);
+            services.AddMetricsReportScheduler();
             services.AddMvc().AddMetrics();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMetricsAllMiddleware();
+            app.UseMetricsAllEndpoints();
             app.UseMvc();
         }
     }
